7c649548f4b80a2b9fe359e9024e7f4a4e4a587c
Владимир Валько <vladimirvalko@MacBook-Air-Vladimir.local>
2022-07-16 12:06:40 +0300

    DB config changed

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ import  handleValidationErrors  from './auth/handleValidationErrors.js';
 import  checkAuth  from './auth/checkAuth.js';
 
 // mongodb+srv://admin:2310714@cluster0.sg3mhrd.mongodb.net/?retryWrites=true&w=majority
-
+// https://exercises-server.herokuapp.com/
 const app = express();
 // const PORT = 4444;
 
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 4444, () => console.log("Server is alive"));
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect('mongodb+srv://admin:2310714@cluster0.sg3mhrd.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log("DB Alive"))
   .catch((err) => console.log("DB Failed", err));
 
 
 
-// app.post("/exercises", ExeciseControllers.pushData); 
-app.get("/exercises", ExeciseControllers.getAll); 
+// app.post("/exercises", ExeciseControllers.pushData);
+app.get("/exercises", ExeciseControllers.getAll);
 app.get("/exercises/:search", ExeciseControllers.searchExercise);
-app.post("/favorite", checkAuth, ExeciseControllers.addToFavorite); 
+app.post("/favorite", checkAuth, ExeciseControllers.addToFavorite);
 
 
 app.post("/auth/register", registerValidation, handleValidationErrors, UserControllers.registrate);
 app.post("/auth/login", loginValidation, handleValidationErrors, UserControllers.login);
 app.get("/auth/me", checkAuth, UserControllers.checkMe);
+
+
+
+app.listen(process.env.PORT || 4444, () => console.log("Server is alive"));
